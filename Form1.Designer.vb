49dbe25760b4abc0b00985cb72c306f18772ad6b
Edina113 <118177307+Edina113@users.noreply.github.com>
2023-03-06 13:23:21 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lstGrad = New System.Windows.Forms.ListBox()
+        Me.cmbSjediste = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(549, 261)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Rezerviši"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(549, 315)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 29)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Izlaz"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(220, 237)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Destinacija"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(396, 237)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Lokacija sjedišta"
+        '
+        'lstGrad
+        '
+        Me.lstGrad.FormattingEnabled = True
+        Me.lstGrad.ItemHeight = 20
+        Me.lstGrad.Location = New System.Drawing.Point(189, 280)
+        Me.lstGrad.Name = "lstGrad"
+        Me.lstGrad.Size = New System.Drawing.Size(150, 104)
+        Me.lstGrad.TabIndex = 4
+        '
+        'cmbSjediste
+        '
+        Me.cmbSjediste.FormattingEnabled = True
+        Me.cmbSjediste.Location = New System.Drawing.Point(377, 280)
+        Me.cmbSjediste.Name = "cmbSjediste"
+        Me.cmbSjediste.Size = New System.Drawing.Size(151, 28)
+        Me.cmbSjediste.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Planer_leta_završeno.My.Resources.Resources.avion2
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cmbSjediste)
+        Me.Controls.Add(Me.lstGrad)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Planer leta"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lstGrad As ListBox
+    Friend WithEvents cmbSjediste As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim zahtjev As String
+        zahtjev = "Destinacija je: " + lstGrad.Text + vbCr
+        zahtjev = zahtjev + "Mjesto sjedenja je: " + cmbSjediste.Text + vbCr
+        MsgBox(zahtjev, vbOKOnly + vbInformation, "Vaš izbor je: ")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstGrad.Items.Add("Sarajevo")
+        lstGrad.Items.Add("Istanbul")
+        lstGrad.Items.Add("Zagreb")
+        lstGrad.Items.Add("Rim")
+        lstGrad.Items.Add("Melburn")
+
+        cmbSjediste.Items.Add("Na početku")
+        cmbSjediste.Items.Add("Na sredini")
+        cmbSjediste.Items.Add("Na kraju")
+
+    End Sub
+End Class
